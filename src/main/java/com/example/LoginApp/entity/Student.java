c0b8dbc06773817f7ed2db5619cc9b69b414f875
avinash <nikitamane2000@gmail.com>
2024-08-14 11:44:38 +0530

    first commit

--- src/main/java/com/example/LoginApp/entity/Student.java
+++ src/main/java/com/example/LoginApp/entity/Student.java
@@ -5,14 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Student {
 	
 	@Id
